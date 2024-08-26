32dbd4cd5c10cabb3f6645d36403d7868ea41ff3
leslieluyu <leslie.luyu@gmail.com>
2024-08-26 02:51:57 +0000

    add overlay for v0.7 norerank  v0.7 cpu .etc

--- ChatQnA/kubernetes/manifests/kustomization/scale.sh
+++ ChatQnA/kubernetes/manifests/kustomization/scale.sh
@@ -89,6 +89,31 @@ scale_tgi8() {
 
 }
 
+scale_tgi7_1() {
+    echo "Begin: scale_tgi7_1"
+    echo "-----------------------"
+    local namespace=$1
+    reset_label
+    reset_deploy "$namespace"
+
+    echo "  label nodes satg-opea-4node-0" 
+    kubectl label nodes satg-opea-4node-0 demo=chatqna
+
+
+    echo "  scale the 4 service to replicas 7/1/1/1" 
+    kubectl -n "$namespace" scale deploy/tgi-gaudi-service-deploy --replicas=7
+    kubectl -n "$namespace" scale deploy/tei-embedding-service-deploy --replicas=1
+    kubectl -n "$namespace" scale deploy/chaqna-xeon-backend-server-deploy --replicas=1
+    kubectl -n "$namespace" scale deploy/tei-reranking-service-deploy --replicas=1
+    #调用reset_label函数
+    echo -e "End: scale_tgi7_1"
+    echo
+    echo
+    get_pods "$namespace"
+
+}
+
+
 scale_tgi16() {
     echo "Begin: scale_tgi16"
     echo "-----------------------"
@@ -107,6 +132,29 @@ scale_tgi16() {
     kubectl -n "$namespace" scale deploy/tei-reranking-service-deploy --replicas=2
     #调用reset_label函数
     echo -e "End: scale_tgi16"
+    echo
+    echo
+    get_pods "$namespace"
+}
+
+scale_tgi15_1() {
+    echo "Begin: scale_tgi15_1"
+    echo "-----------------------"
+    local namespace=$1
+    reset_label
+    reset_deploy "$namespace"
+
+    echo "  label nodes satg-opea-4node-0/1" 
+    kubectl label nodes satg-opea-4node-0 demo=chatqna
+    kubectl label nodes satg-opea-4node-1 demo=chatqna
+
+    echo "  scale the 4 service to replicas 15/2/2/1" 
+    kubectl -n "$namespace" scale deploy/tgi-gaudi-service-deploy --replicas=15
+    kubectl -n "$namespace" scale deploy/tei-embedding-service-deploy --replicas=2
+    kubectl -n "$namespace" scale deploy/chaqna-xeon-backend-server-deploy --replicas=2
+    kubectl -n "$namespace" scale deploy/tei-reranking-service-deploy --replicas=1
+    #调用reset_label函数
+    echo -e "End: scale_tgi15_1"
     echo
     echo
     get_pods "$namespace"
@@ -132,6 +180,31 @@ scale_tgi32() {
     kubectl -n "$namespace" scale deploy/tei-reranking-service-deploy --replicas=4
     #调用reset_label函数
     echo -e "End: scale_tgi32"
+    echo
+    echo
+    get_pods "$namespace"
+}
+
+scale_tgi31_1() {
+    echo "Begin: scale_tgi31_1"
+    echo "-----------------------"
+    local namespace=$1
+    reset_label
+    reset_deploy "$namespace"
+
+    echo "  label nodes satg-opea-4node-0/1/2/3" 
+    kubectl label nodes satg-opea-4node-0 demo=chatqna
+    kubectl label nodes satg-opea-4node-1 demo=chatqna
+    kubectl label nodes satg-opea-4node-2 demo=chatqna
+    kubectl label nodes satg-opea-4node-3 demo=chatqna
+
+    echo "  scale the 4 service to replicas 31/4/4/1" 
+    kubectl -n "$namespace" scale deploy/tgi-gaudi-service-deploy --replicas=31
+    kubectl -n "$namespace" scale deploy/tei-embedding-service-deploy --replicas=4
+    kubectl -n "$namespace" scale deploy/chaqna-xeon-backend-server-deploy --replicas=4
+    kubectl -n "$namespace" scale deploy/tei-reranking-service-deploy --replicas=1
+    #调用reset_label函数
+    echo -e "End: scale_tgi31_1"
     echo
     echo
     get_pods "$namespace"
@@ -178,6 +251,15 @@ main() {
             ;;
         tgi32)
             scale_tgi32 "$namespace"
+            ;;
+        tgi7_1)
+            scale_tgi7_1 "$namespace"
+            ;;
+        tgi15_1)
+            scale_tgi15_1 "$namespace"
+            ;;
+        tgi31_1)
+            scale_tgi31_1 "$namespace"
             ;;
         all)
             scale_tgi1 "$namespace"
